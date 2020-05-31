a69795a4522fd495a27718ac8a00e83e2a5fec53
knfjed <knfjed@gmail.com>
2020-05-31 20:17:44 +0900

    React Hooks useMemo

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useMemo } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
-  useEffect(() => {
-    console.log("useEffect");
-  }, []);
+  const dubble = (count) => {
+    let i = 0;
+    while (i < 10000000) i++;
+    return count * 2;
+  };
+
+  const dubbleCount = useMemo(() => dubble(count1), [count1]);
 
   return (
     <div>
-      <p id="effectHook">You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>click</button>
+      <p>count1: {count1}</p>
+      <p>dubbleCount: {dubbleCount}</p>
+      <button onClick={() => setCount1(count1 + 1)}>increment1</button>
+
+      <p>count2: {count2}</p>
+      <button onClick={() => setCount2(count2 + 1)}>increment2</button>
     </div>
   );
 }
